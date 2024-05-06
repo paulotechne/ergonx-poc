a7e2d55411ec3d00ed0f53f844c52e806117f027
paulo.gonzaga <paulo.gonzaga@techne.com.br>
2024-05-06 11:16:05 -0300

    config: Configurações iniciais do projeto

---
+++ src/main/java/com/example/ergonxpoc/ErgonxPocApplication.java
@@ -0,0 +1,13 @@
+package com.example.ergonxpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErgonxPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErgonxPocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ergonxpoc/ErgonxPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ergonxpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErgonxPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
